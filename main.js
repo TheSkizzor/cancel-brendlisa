39365a4608e4128d1253e785ff90b1a6e5019008
TheSkizzor <73627273+TheSkizzor@users.noreply.github.com>
2020-11-17 06:20:30 -0500

    Initial upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const cancelBtn = document.querySelector('#cancel')
+const bg = document.querySelector('#bg')
+const main = document.querySelector('#main')
+const nav = document.querySelector('#nav')
+
+// BUTTON ANIMATION
+animateOut()
+cancelBtn.addEventListener('mouseover', animateIn)
+cancelBtn.addEventListener('mouseout', animateOut)
+function animateIn() {
+	bg.style.filter = 'brightness(0.4)'
+	main.style.backdropFilter = 'blur(4px)'
+}
+function animateOut() {
+	bg.style.filter = 'brightness(0.7)'
+	main.style.backdropFilter = 'blur(1px)'
+}
+
+// STICKY NAV
+window.onscroll = stickyNav;
+const stickPos = nav.offsetTop;
+function stickyNav() {
+  if (window.pageYOffset >= stickPos) {
+		nav.classList.add("sticky")
+  } else {
+		nav.classList.remove("sticky");
+  }
+}
