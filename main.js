425f8c59721087591b0a40bd53bb464063f9041f
TheSkizzor <73627273+TheSkizzor@users.noreply.github.com>
2020-11-17 06:37:45 -0500

    Minor changes

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const bg = document.querySelector('#bg')
 const main = document.querySelector('#main')
 const nav = document.querySelector('#nav')
 
+// STICKY NAV
+window.onscroll = stickyNav;
+const stickPos = nav.offsetTop;
+function stickyNav() {
+  if (window.pageYOffset >= stickPos) {
+		nav.classList.add("sticky")
+  } else {
+		nav.classList.remove("sticky")
+  }
+}
+
 // BUTTON ANIMATION
 animateOut()
 cancelBtn.addEventListener('mouseover', animateIn)
@@ -16,13 +27,7 @@ function animateOut() {
 	main.style.backdropFilter = 'blur(1px)'
 }
 
-// STICKY NAV
-window.onscroll = stickyNav;
-const stickPos = nav.offsetTop;
-function stickyNav() {
-  if (window.pageYOffset >= stickPos) {
-		nav.classList.add("sticky")
-  } else {
-		nav.classList.remove("sticky");
-  }
-}
+// BUTTON ONCLICK
+cancelBtn.addEventListener('click', () => {
+	window.location.href = '#test'
+})
